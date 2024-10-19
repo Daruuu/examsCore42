9837e76485942ac450b6e48120b9f026ccc5f303
Daruuu <thedaruny16@gmail.com>
2024-10-19 22:38:50 +0200

    level 01 all okay

---
+++ last_one/nivel01/repeat_alpha/repeat_alpha.c
@@ -0,0 +1,62 @@
+/*
+Assignment name  : repeat_alpha
+Expected files   : repeat_alpha.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program called repeat_alpha that takes a string and display it
+repeating each alphabetical character as many times as its alphabetical index,
+followed by a newline.
+
+'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
+
+Case remains unchanged.
+
+If the number of arguments is not 1, just display a newline.
+
+Examples:
+
+$>./repeat_alpha "abc"
+abbccc
+$>./repeat_alpha "Alex." | cat -e
+Alllllllllllleeeeexxxxxxxxxxxxxxxxxxxxxxxx.$
+$>./repeat_alpha 'abacadaba 42!' | cat -e
+abbacccaddddabba 42!$
+$>./repeat_alpha | cat -e
+$
+$>
+$>./repeat_alpha "" | cat -e
+$
+$>
+*/
+
+#include <unistd.h>
+
+void    repeat(char *str)
+{
+    int i = 0;
+    int count = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            count = str[i] - 'a' + 1;
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            count = str[i] - 'A' + 1;
+        else
+            count = 1;
+        while (count --)
+            write(1, &str[i], 1);
+        i ++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+    {
+        repeat(av[1]);
+    }
+    write(1, "\n", 1);
+    return (0);
+}
