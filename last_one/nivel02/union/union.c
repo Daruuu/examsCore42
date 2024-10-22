ecf2a0a5cd956b5b62523b197382ecce2ed85e2b
daruuu <thedaruny16@gmail.com>
2024-10-22 09:00:58 +0200

    level 2 -> 10-15

---
+++ last_one/nivel02/union/union.c
@@ -0,0 +1,67 @@
+/*
+Assignment name  : union
+		Expected files   : union.c
+		Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+
+The display will be in the order characters appear in the command line, and
+		will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Example:
+
+$>./union zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+		zpadintoqefwjy$
+$>./union ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+		df6vewg4thras$
+$>./union "rien" "cette phrase ne cache rien" | cat -e
+		rienct phas$
+		$>./union | cat -e
+		$
+$>
+$>./union "rien" | cat -e
+		$
+$>*/
+#include <unistd.h>
+
+void	unio(char *s1, char *s2)
+{
+	char	ascii_table[256] = {0};
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		if (!ascii_table[(unsigned char)s1[i]])
+		{
+			write(1, &s1[i], 1);
+			ascii_table[(unsigned char)s1[i]] = 1;
+		}
+		i ++;
+	}
+	while (s2[j] !='\0')
+	{
+		if (!ascii_table[(unsigned char)s2[j]])
+		{
+			write(1, &s2[j], 1);
+			ascii_table[(unsigned char)s2[j]] = 1;
+		}
+		j ++;
+	}
+}
+
+int main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		unio(av[1], av[2]);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
