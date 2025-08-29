6197bb32342bb3912ed09b4a75710b5932f5ee39
Daruuu <thedaruny16@gmail.com>
2025-08-29 18:43:54 +0200

    rm versions that not work & add popen that pass exam test

--- exam_rank04/level01/ft_popen/ft_popen_exam.c
+++ exam_rank04/level01/ft_popen/ft_popen_exam.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
-int ft_popen(const char *file, char *const argv[], char type)
+int	ft_popen(const char *file, char *const argv[], char type)
 {
-	if (!file || !argv || (type != 'r' && type != 'w'))
+	if (!file || !argv || (type != 'r' && type !- 'w'))
 		return (-1);
+
 	int	fd[2];
 	int	pid;
+	
 	if (pipe(fd) < 0)
 		return (-1);
 	pid = fork();
@@ -17,31 +20,32 @@ int ft_popen(const char *file, char *const argv[], char type)
 		close(fd[1]);
 		return (-1);
 	}
-	if (pid == 0)
+	if (pid == 0)	//child process
 	{
 		if (type == 'r')
 		{
 			if (dup2(fd[1], STDOUT_FILENO) < 0)
-				exit (1);
+				exit(1);
 		}
 		if (type == 'w')
 		{
 			if (dup2(fd[0], STDIN_FILENO) < 0)
-				exit (1);
+				exit(1);
 		}
 		close(fd[0]);
 		close(fd[1]);
-		execvp(file, argv);
-		exit(1);
+		if (execvp(file, argv) == -1)
+			exit(1);
 	}
+	// father process
 	if (type == 'r')
 	{
-		close(fd[1]);
+		close(fd[1]);	//close escritura, me quedo con lectura
 		return (fd[0]);
 	}
 	if (type == 'w')
 	{
-		close(fd[0]);
+		close(fd[0]);	//	cierro lectura, me quedo con escritura
 		return (fd[1]);
 	}
 	return (0);
@@ -55,12 +59,14 @@ int	main()
 
 	fd = ft_popen("ls", (char *const[]){"ls", "-l", NULL}, 'r');
 	if (fd == -1)
-		return(1);
+		return (1);
+	
 	while ((bytes = read(fd, buffer, sizeof(buffer) - 1)) > 0)
 	{
 		buffer[bytes] = '\0';
 		printf("%s", buffer);
 	}
 	close(fd);
+
 	return (0);
 }
